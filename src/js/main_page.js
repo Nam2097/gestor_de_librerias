22998df6ab8304aa9aa2d46d87ee4e6fd36fb301
andgonzalezcas <andgonzalezcas@unal.edu.co>
2021-02-17 20:46:11 -0500

    Se inicio con el proyecto, el cual ya recibe datos de un archivo .json

---
+++ src/js/main_page.js
@@ -0,0 +1,25 @@
+document.querySelector('#load_json_button').addEventListener('click', trearDatos)
+
+function trearDatos(){
+    const xhttp = new XMLHttpRequest()
+    
+    xhttp.open('GET', './src/json/biblioteca.json', true)
+    xhttp.send()
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200){
+            let data = JSON.parse(this.responseText)
+
+            let tableContents = document.querySelector('#table__contents')
+            tableContents.innerHTML = ''
+            for(let item of data){
+                tableContents.innerHTML += `
+                    <tr>
+                        <th>${item.Libro}</th>
+                        <th>${item.Escritor}</th>
+                        <th>${item.Leido}</th>
+                    </tr>
+                `
+            }
+        }
+    }
+}
