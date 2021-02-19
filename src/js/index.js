e34b094acffeca3d334d37e85d1f5a6d1a973c7d
andgonzalezcas <andgonzalezcas@unal.edu.co>
2021-02-19 18:21:40 -0500

    Se volvio a la version 4391c85baf919b79636f5c70fd41133db1262aa9 de forma brusca ya que no encontre otra forma de hacerlo

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ function redireccionar(){
 document.getElementById('modal_button').addEventListener("click", function() {
     username = document.getElementById('username')
     if (username.value != ''){
-        redireccionar()
+        document.getElementById('background_modal_section').style.display = 'none'
     } else {
         document.getElementById('modal_warning').innerHTML = 'Por favor, dime como quieres que te llamemos'
     }
