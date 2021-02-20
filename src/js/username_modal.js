9ef82d6626319b12940a1b1f2f21400bc6cc5006
andgonzalezcas <andgonzalezcas@unal.edu.co>
2021-02-20 12:42:05 -0500

    Se realizo un cambio volviendo a un solo index el cual tiene un modal que funciona como ingreso de username y una main page donde se encuentra el administrador json funcional

--- src/js/username_modal.js
+++ src/js/username_modal.js
@@ -1,14 +1,11 @@
 /* GLOBAL VARIABLES */
 let username = ''
 
-function redireccionar(){
-    window.location="./pages/main_page.html";
-}
-
+/* USERNAME MODAL */
 document.getElementById('modal_button').addEventListener("click", function() {
     username = document.getElementById('username')
     if (username.value != ''){
-        document.getElementById('background_modal_section').style.display = 'none'
+        document.querySelector('.background_username_modal').style.display = 'none'
     } else {
         document.getElementById('modal_warning').innerHTML = 'Por favor, dime como quieres que te llamemos'
     }
